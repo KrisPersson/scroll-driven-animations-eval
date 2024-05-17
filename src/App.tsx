680c340b8093f534890386ac0ae4dc909dc5e3b1
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-05-17 12:44:29 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,74 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1 className="heading-class">Scroll-Driven Animation</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aut
+        dolorem est? Quis necessitatibus sint labore ab perferendis et, deserunt
+        exercitationem repellat ad, voluptatibus tempora, eum rerum eius magnam
+        aperiam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repellat maxime qui similique corrupti voluptatibus voluptate
+        repudiandae magnam consequatur atque, accusantium itaque ea molestias
+        natus adipisci assumenda autem distinctio minima saepe, nam accusamus id
+        suscipit doloribus dolorum. Iure eius ratione, dolores assumenda fugiat
+        fugit consequuntur, molestias recusandae dolore veritatis iusto vitae?
+      </p>
+      <div className="img-wrapper">
+        <img className="img-class" src="/01.jpg" />
+      </div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio aliquam
+        dicta, corporis quo sunt dolore in alias perspiciatis obcaecati
+        accusamus. Quaerat, optio ullam temporibus provident ipsum architecto
+        assumenda, magni at beatae quibusdam nulla asperiores tempora dolores
+        consectetur, doloribus consequuntur. Error.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aut
+        dolorem est? Quis necessitatibus sint labore ab perferendis et, deserunt
+        exercitationem repellat ad, voluptatibus tempora, eum rerum eius magnam
+        aperiam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repellat maxime qui similique corrupti voluptatibus voluptate
+        repudiandae magnam consequatur atque, accusantium itaque ea molestias
+        natus adipisci assumenda autem distinctio minima saepe, nam accusamus id
+        suscipit doloribus dolorum. Iure eius ratione, dolores assumenda fugiat
+        fugit consequuntur, molestias recusandae dolore veritatis iusto vitae?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio aliquam
+        dicta, corporis quo sunt dolore in alias perspiciatis obcaecati
+        accusamus. Quaerat, optio ullam temporibus provident ipsum architecto
+        assumenda, magni at beatae quibusdam nulla asperiores tempora dolores
+        consectetur, doloribus consequuntur. Error.
+      </p>
+      <div className="img-wrapper">
+        <img className="img-class" src="/02.jpg" />
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aut
+        dolorem est? Quis necessitatibus sint labore ab perferendis et, deserunt
+        exercitationem repellat ad, voluptatibus tempora, eum rerum eius magnam
+        aperiam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repellat maxime qui similique corrupti voluptatibus voluptate
+        repudiandae magnam consequatur atque, accusantium itaque ea molestias
+        natus adipisci assumenda autem distinctio minima saepe, nam accusamus id
+        suscipit doloribus dolorum. Iure eius ratione, dolores assumenda fugiat
+        fugit consequuntur, molestias recusandae dolore veritatis iusto vitae?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio aliquam
+        dicta, corporis quo sunt dolore in alias perspiciatis obcaecati
+        accusamus. Quaerat, optio ullam temporibus provident ipsum architecto
+        assumenda, magni at beatae quibusdam nulla asperiores tempora dolores
+        consectetur, doloribus consequuntur. Error.
+      </p>
+      <div className="img-wrapper">
+        <img className="img-class" src="/03.jpg" />
+      </div>
+    </main>
+  );
+}
+
+export default App;
